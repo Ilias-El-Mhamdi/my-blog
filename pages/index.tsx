f58844184f54a8bac0a7ac6efda6252e228a4f3e
ilias <ilias.elmhamdi@gmail.com>
2023-06-09 01:27:09 +0200

    working tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
+
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hi, I&apos; Ilias El-Mhamdi. This blog exists to organize my LinkedIn articles for easy access. (This is a first draft, still in progress) 👋</h1>
+    <h1  className='text-red-800' >Hi, I&apos; Ilias El-Mhamdi. This blog exists to organize my LinkedIn articles for easy access. (This is a first draft, still in progress) 👋</h1>
     <p>
       <Link href="/about">
         <a>About</a>
