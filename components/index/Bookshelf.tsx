ac3c4de61b9b3b25a1da0f1a8dc5ff40c18fe1b3
ilias <ilias.elmhamdi@gmail.com>
2023-06-09 21:28:53 +0200

    search bar filter

--- components/index/Bookshelf.tsx
+++ components/index/Bookshelf.tsx
@@ -1,8 +1,27 @@
 /* eslint-disable @next/next/no-img-element */
 import { bookShelf } from '../../data/articles';
 import { Article } from './Article';
+import { useState } from 'react';
 
 export function Bookshelf() {
+
+  const [filteredBookShelf, setFilteredBookShelf] = useState(bookShelf);
+
+  const handleSearch = (e) => {
+    if (e.target.value.length === 0) {
+      setFilteredBookShelf(bookShelf);
+    } else {
+      const filtered = bookShelf.map(shelf => {
+        return {
+          ...shelf,
+          articles: shelf.articles.filter(article => article.title.toLocaleLowerCase().includes(e.target.value.toLocaleLowerCase())),
+        };
+      });
+      setFilteredBookShelf(filtered);
+    }
+  };
+
+
   return <>
     <div className='m-4'>
       <h2 className='text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>Articles</h2>
@@ -10,16 +29,17 @@ export function Bookshelf() {
         <img
           src='https://static.thenounproject.com/png/424995-200.png'
           alt='' className='w-8 h-8  w-24 h-24 absolute top-1.5 left-2 ' />
-        <input className='rounded-2xl p-2 border-2 w-full pl-9' />
+        <input onInput={handleSearch} className='rounded-2xl p-2 border-2 w-full pl-9' />
       </div>
-      {bookShelf.map(shelf => <>
-        <hr className='m-4' />
-        <h2
-          className='text-2xl font-bold tracking-tight text-gray-900 dark:text-white capitalize mb-4'>{shelf.book.title.toLocaleLowerCase()}</h2>
-        <div className='flex flex-wrap gap-4'>
-          {shelf.articles.map(article => (<Article key={article.url} article={article} />))}
-        </div>
-      </>)}
+      {filteredBookShelf.map(shelf => shelf.articles.length ? <>
+          <hr className='m-4' />
+          <h2
+            className='text-2xl font-bold tracking-tight text-gray-900 dark:text-white capitalize mb-4'>{shelf.book.title.toLocaleLowerCase()}</h2>
+          <div className='flex flex-wrap gap-4'>
+            {shelf.articles.map(article => (<Article key={article.url} article={article} />))}
+          </div>
+        </> :
+        <></>)}
     </div>
   </>;
 }
