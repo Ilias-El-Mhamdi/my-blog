98a8301ce8bf10d56a83baabaabfa5973c1474ca
ilias <ilias.elmhamdi@gmail.com>
2023-06-09 20:17:58 +0200

    add articles

---
+++ data/articles.ts
@@ -0,0 +1,337 @@
+const books = {
+  cc: { title: 'CLEAN CODE', img: '/covers/clean_code.jpg' },
+  cAr: { title: 'CLEAN ARCHITECTURE', img: '/covers/clean_archi.jpg' },
+  cAg: { title: 'CLEAN AGILE', img: '/covers/clean_agile.jpg' },
+  xp: { title: 'EXTREME PROGRAMMING EXPLAINED', img: '/covers/xp.jpg' },
+  sc: { title: 'SOFTWARE CRAFT', img: '/covers/software_craft.jpg' },
+  te: { title: 'TECH EXCELLENCE', img: '/covers/tech_exc.jpg' },
+  rrg: { title: 'REFACTORING (REFACTORING GURU)', img: '/covers/refactoring_guru.png' },
+  dprg: { title: 'DESIGN PATTERNS (REFACTORING GURU)', img: '/covers/refactoring_guru.png' },
+};
+
+export const techExcArticles = [
+  {
+    book: books.te,
+    title: '[SUMMARY] Getting out of Legacy with 3P (Johan Martinsson)',
+    url: 'https://www.linkedin.com/pulse/summary-getting-out-legacy-3p-johan-martinsson-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.te,
+    title: '[SUMMARY] TDD Anti-patterns (Ahmed Misbah)',
+    url: 'https://www.linkedin.com/pulse/summary-tdd-anti-patterns-ahmed-misbah-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.te,
+    title: '[SUMMARY] Engineering Leaders: Creating a culture of Tech Excellence (Ariel Pérez)',
+    url: 'https://www.linkedin.com/pulse/summary-engineering-leaders-creating-culture-tech-ariel-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.te,
+    title: '[SUMMARY] TDD and Remote Mob Programming (Marco Consolaro & Alessandro Di Gioia)',
+    url: 'https://www.linkedin.com/pulse/summary-tdd-remote-mob-programming-marco-consolaro-di-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.te,
+    title: 'The key concept in computer science and how it relates to ATDD (Peter Gfader)',
+    url: 'https://www.linkedin.com/pulse/key-concept-computer-science-how-relates-atdd-peter-gfader-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.te,
+    title: 'Bigger Unit Tests are Better (Peter Schuler)',
+    url: 'https://www.linkedin.com/pulse/bigger-unit-tests-better-peter-schuler-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.te,
+    title: '[SUMMARY] Improving your integration testing efforts with contract testing (Bas Dijkstra)',
+    url: 'https://www.linkedin.com/pulse/summary-improving-your-integration-testing-efforts-bas-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.te,
+    title: '[SUMMARY] Tech Interview Cookbook (Rodislav Moldovan and Adrien Muller)',
+    url: 'https://www.linkedin.com/pulse/summary-tech-interview-cookbook-rodislav-moldovan-adrien-el-mhamdi?trk=portfolio_article-card_title',
+  },
+
+  {
+    book: books.te,
+    title: '[SUMMARY] The Trouble with Continuous Delivery (Bryan Finster)',
+    url: 'https://www.linkedin.com/pulse/summary-trouble-continuous-delivery-bryan-finster-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+
+  {
+    book: books.te,
+    title: '[SUMMARY] Agile is a slow and steady way to win the race (Alex Bunardzic)',
+    url: 'https://www.linkedin.com/pulse/summary-agile-slow-steady-way-win-race-alex-bunardzic-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+
+
+];
+export const cleanCodeArticles = [
+  {
+    book: books.cc,
+    title: '[SUMMARY] Clean Code Chapter 1: There Will Be Code (Robert C. Martin)',
+    url: 'https://www.linkedin.com/posts/ilias-el-mhamdi-72a013146_cleancode-tdd-cleanarchitecture-activity-6974038694475272192-WR-u?utm_source=share&utm_medium=member_desktop',
+  },
+  {
+    book: books.cc,
+    title: '[SUMMARY] Clean Code Chapter 2: Meaningful Names (Robert C. Martin)',
+    url: 'https://www.linkedin.com/posts/ilias-el-mhamdi-72a013146_cleancode-cleanarchitecture-developpeur-activity-6974310028803465216-Q4-2?utm_source=share&utm_medium=member_desktop',
+  },
+  {
+    book: books.cc,
+    title: '[SUMMARY] Clean Code Chapter 3: Functions (Robert C. Martin)',
+    url: 'https://www.linkedin.com/pulse/summary-clean-code-chapter-3-functions-robert-c-martin-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.cc,
+    title: '[SUMMARY] Clean Code Chapter 4: Comments (Robert C. Martin)',
+    url: 'https://www.linkedin.com/pulse/summary-clean-code-chapter-4-comments-robert-c-martin-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+
+  {
+    book: books.cc,
+    title: '[SUMMARY] Clean Code Chapter 5: Formatting (Robert C. Martin)',
+    url: 'https://www.linkedin.com/pulse/summary-clean-code-chapter-5-formatting-robert-c-martin-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.cc,
+    title: '[SUMMARY] Clean Code Chapter 6: Objects and Data Structures (Robert C. Martin)',
+    url: 'https://www.linkedin.com/pulse/summary-clean-code-chapter-6-objects-data-structures-robert-ilias?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.cc,
+    title: '[SUMMARY] Clean Code Chapter 7: Error Handling (Robert C. Martin)',
+    url: 'https://www.linkedin.com/pulse/summary-clean-code-chapter-7-error-handling-robert-c-martin-ilias?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.cc,
+    title: '[SUMMARY] Clean Code Chapter 8: Boundaries (Robert C. Martin)',
+    url: 'https://www.linkedin.com/pulse/summary-clean-code-chapter-8-boundaries-robert-c-martin-el-mhamdi?trk=portfolio_article-card_title',
+  },
+
+  {
+    book: books.cc,
+    title: '[SUMMARY] Clean Code Chapter 9: Unit Tests (Robert C. Martin)',
+    url: 'https://www.linkedin.com/pulse/summary-clean-code-chapter-9-unit-tests-robert-c-martin-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.cc,
+    title: '[SUMMARY] Clean Code Chapter 10: Classes (Robert C. Martin)',
+    url: 'https://www.linkedin.com/pulse/summary-clean-code-chapter-10-classes-robert-c-martin-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.cc,
+    title: '[SUMMARY] Clean Code Chapter 11: Systems (Robert C. Martin)',
+    url: 'https://www.linkedin.com/pulse/summary-clean-code-chapter-11-systems-robert-c-martin-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.cc,
+    title: '[SUMMARY] Clean Code Chapter 12: Emergence (Robert C. Martin)',
+    url: 'https://www.linkedin.com/pulse/summary-clean-code-chapter-12-emergence-robert-c-martin-el-mhamdi?trk=portfolio_article-card_title',
+  },
+
+];
+export const softwareCraftArticles = [
+  {
+    book: books.sc,
+    title: '[SUMMARY] Software Craft - Introduction',
+    url: 'https://www.linkedin.com/pulse/summary-software-craft-introduction-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+
+  {
+    book: books.sc,
+    title: '[SUMMARY] Software Craft - Chapter 1: TDD',
+    url: 'https://www.linkedin.com/pulse/summary-software-craft-chapter-1-tdd-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.sc,
+    title: '[SUMMARY] Software Craft - Chapter 2: Clean Code',
+    url: 'https://www.linkedin.com/pulse/summary-software-craft-chapter-2-clean-code-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.sc,
+    title: '[SUMMARY] Software Craft - Chapter 3: BDD',
+    url: 'https://www.linkedin.com/pulse/summary-software-craft-chapter-3-bdd-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.sc,
+    title: '[SUMMARY] Software Craft - Chapter 4: Collaboration',
+    url: 'https://www.linkedin.com/pulse/summary-software-craft-chapter-4-collaboration-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.sc,
+    title: '[SUMMARY] Software Craft - Chapter 5: Refactoring',
+    url: 'https://www.linkedin.com/pulse/summary-software-craft-chapter-5-refactoring-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+
+  {
+    book: books.sc,
+    title: '[SUMMARY] Software Craft - Chapter 6: Working With Legacy Code',
+    url: 'https://www.linkedin.com/pulse/summary-software-craft-chapter-6-working-legacy-code-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.sc,
+    title: '[SUMMARY] Software Craft - Chapter 7: Fraction Kata',
+    url: 'https://www.linkedin.com/pulse/summary-software-craft-chapter-7-fraction-kata-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.sc,
+    title: '[SUMMARY] Software Craft - Chapter 8 (1/2): Test Doubles',
+    url: 'https://www.linkedin.com/pulse/summary-software-craft-chapter-8-12-test-doubles-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.sc,
+    title: '[SUMMARY] Software Craft - Chapter 8 (2/2): The Testing Pyramid',
+    url: 'https://www.linkedin.com/pulse/summary-software-craft-chapter-8-22-testing-pyramid-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.sc,
+    title: '[SUMMARY] Software Craft - Chapter 9: Flavors of TDD',
+    url: 'https://www.linkedin.com/pulse/summary-software-craft-chapter-9-flavors-tdd-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.sc,
+    title: '[SUMMARY] Software Craft - Chapter 10: Design Principles and Patterns',
+    url: 'https://www.linkedin.com/pulse/summary-software-craft-chapter-10-design-principles-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.sc,
+    title: '[SUMMARY] Software Craft - Chapter 11: Architecture',
+    url: 'https://www.linkedin.com/pulse/summary-software-craft-chapter-11-architecture-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.sc,
+    title: '[SUMMARY] Software Craft - Chapter 12: Craft Initiation',
+    url: 'https://www.linkedin.com/pulse/summary-software-craft-chapter-12-initiation-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.sc,
+    title: '[SUMMARY] Software Craft - Chapter 13 & 14: Mindset',
+    url: 'https://www.linkedin.com/pulse/summary-software-craft-chapter-13-14-mindset-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+
+];
+export const xpArticles = [
+  {
+    book: books.xp,
+    title: '[SUMMARY] Extreme programming Explained by Kent Beck : Values and Principles',
+    url: 'https://www.linkedin.com/pulse/summary-extreme-programming-explained-kent-beck-values-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.xp,
+    title: '[SUMMARY] Extreme programming Explained by Kent Beck : Practices',
+    url: 'https://www.linkedin.com/pulse/summary-extreme-programming-explained-kent-beck-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+
+
+];
+export const cleanAgileArticles = [
+  {
+    book: books.cAg,
+    title: '[SUMMARY] Clean Agile - Chapter 1 : Introduction to Agile',
+    url: 'https://www.linkedin.com/pulse/summary-clean-agile-chapter-1-introduction-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.cAg,
+    title: '[SUMMARY] Clean Agile - Chapter 2 : The Reasons for Agile',
+    url: 'https://www.linkedin.com/pulse/summary-clean-agile-chapter-2-reasons-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+
+  {
+    book: books.cAg,
+    title: '[SUMMARY] Clean Agile - Chapter 3 : Business Practices',
+    url: 'https://www.linkedin.com/pulse/summary-clean-agile-chapter-3-business-practices-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+
+  {
+    book: books.cAg,
+    title: '[SUMMARY] Clean Agile - Chapter 4 : Team Practices',
+    url: 'https://www.linkedin.com/pulse/summary-clean-agile-chapter-4-team-practices-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.cAg,
+    title: '[SUMMARY] Clean Agile - Chapter 5 : Technical Practices',
+    url: 'https://www.linkedin.com/pulse/summary-clean-agile-chapter-4-technical-practices-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.cAg,
+    title: '[SUMMARY] Clean Agile - Chapter 7 : Craftsmanship',
+    url: 'https://www.linkedin.com/pulse/summary-clean-agile-chapter-7-craftsmanship-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+
+];
+export const refactoringRgArticles = [
+  {
+    book: books.rrg,
+    title: 'Refactoring: Introduction',
+    url: 'https://www.linkedin.com/pulse/refactoring-introduction-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.rrg,
+    title: 'Refactoring: Code smells',
+    url: 'https://www.linkedin.com/pulse/refactoring-code-smells-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.rrg,
+    title: 'Refactoring: Composing Methods',
+    url: 'https://www.linkedin.com/pulse/refactoring-composing-methods-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.rrg,
+    title: 'Refactoring: Moving Features between Objects',
+    url: 'https://www.linkedin.com/pulse/refactoring-moving-features-between-objects-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.rrg,
+    title: 'Refactoring: Organizing Data',
+    url: 'https://www.linkedin.com/pulse/refactoring-organizing-data-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+  {
+    book: books.rrg,
+    title: 'Refactoring: Simplifying Method Calls',
+    url: 'https://www.linkedin.com/pulse/refactoring-simplifying-method-calls-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+];
+export const designPatternsRgArticles = [
+  {
+    book: books.dprg,
+    title: 'Design Patterns: Introduction',
+    url: 'https://www.linkedin.com/pulse/design-patterns-introduction-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+
+  {
+    book: books.dprg,
+    title: 'Design Patterns: Creational Patterns',
+    url: 'https://www.linkedin.com/pulse/design-patterns-creational-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+
+  {
+    book: books.dprg,
+    title: 'Design Patterns: Structural Patterns',
+    url: 'https://www.linkedin.com/pulse/design-patterns-structural-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+
+  {
+    book: books.dprg,
+    title: 'Design Patterns: Behavioral Patterns (1/2)',
+    url: 'https://www.linkedin.com/pulse/design-patterns-behavioral-12-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+
+
+  {
+    book: books.dprg,
+    title: 'Design Patterns: Behavioral Patterns (2/2)',
+    url: 'https://www.linkedin.com/pulse/design-patterns-behavioral-22-ilias-el-mhamdi?trk=portfolio_article-card_title',
+  },
+
+
+];
+
+export const allArticles = [
+  { articles: techExcArticles },
+  { articles: cleanCodeArticles },
+  { articles: softwareCraftArticles },
+  { articles: xpArticles },
+  { articles: cleanAgileArticles },
+  { articles: refactoringRgArticles },
+  { articles: designPatternsRgArticles },
+];
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,30 @@
+/* eslint-disable @next/next/no-img-element */
 import Link from 'next/link';
 import Layout from '../components/Layout';
 import Image from 'next/image';
+import { allArticles } from '../data/articles';
 
+
+const Article = ({ article }) => (
+  <Link href={article.url}>
+    <a className='underline hover:text-blue-800' target='_blank'>
+      <div
+        className='max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 h-full '>
+
+        <img className='rounded-t-lg'
+             src={article.book.img} alt='' />
+
+        <div className='p-5'>
+          <h5
+            className='hover:text-blue-800 mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>{article.title}</h5>
+        </div>
+      </div>
+    </a>
+  </Link>
+);
 
 const IndexPage = () => (
-  <Layout title='Home | Next.js + TypeScript Example'>
+  <Layout>
     <div className='flex items-center gap-2'>
       <Image className={'rounded-full'} src='/me.jpg' width={100} height={100} alt={'me'}></Image>
       <h1 className=''>Hi 👋, I&apos;m Ilias El-Mhamdi <br /> This blog exists to organize my LinkedIn articles for easy
@@ -22,6 +42,21 @@ const IndexPage = () => (
     <Link href={'/cv.pdf'}>
       <a className='underline hover:text-blue-800' target='_blank'>My CV</a>
     </Link>
+
+    <div className='m-4'>
+      <h2 className='text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>Articles</h2>
+      {allArticles.map(book =>
+        <>
+          <hr className='m-4' />
+          <h2
+            className='text-2xl font-bold tracking-tight text-gray-900 dark:text-white capitalize mb-4'>{book.articles[0].book.title.toLocaleLowerCase()}</h2>
+          <div className='flex flex-wrap gap-4'>
+            {book.articles.map(article => (<Article key={article.url} article={article} />))}
+          </div>
+        </>,
+      )}
+    </div>
+
 
   </Layout>
 );
