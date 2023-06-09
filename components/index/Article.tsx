ee29f7d8a7135eecc59b0fed238009a6bb379fb9
ilias <ilias.elmhamdi@gmail.com>
2023-06-09 22:52:23 +0200

    search bar filter

--- components/index/Article.tsx
+++ components/index/Article.tsx
@@ -3,16 +3,16 @@ import Link from 'next/link';
 
 export const Article = ({ article }) => (
   <Link href={article.url}>
-    <a className='underline hover:text-blue-800' target='_blank'>
+    <a className='underline hover:text-blue-800 text-gray-900' target='_blank'>
       <div
-        className='max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 h-full '>
+        className='max-w-xs bg-white border border-gray-200 rounded-lg shadow h-full '>
 
         <img className='rounded-t-lg'
              src={article.book.img} alt='' />
 
         <div className='p-5'>
           <h5
-            className='hover:text-blue-800 mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>{article.title}</h5>
+            className='hover:text-blue-800 mb-2 text-2xl font-bold tracking-tight'>{article.title}</h5>
         </div>
       </div>
     </a>
